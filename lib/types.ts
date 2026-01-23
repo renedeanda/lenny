225cb8c5f26d96305b4422840f28bc220d0d6f0d
renedeanda <rene.ideanda@gmail.com>
2026-01-22 19:58:56 -0800

    fix build error

--- lib/types.ts
+++ lib/types.ts
@@ -1,4 +1,4 @@
-export type QuestionId = 'q1' | 'q2' | 'q3' | 'q4' | 'q5' | 'q6' | 'q7';
+export type QuestionId = 'q1' | 'q2' | 'q3' | 'q4' | 'q5' | 'q6' | 'q7' | 'q8' | 'q9' | 'q10';
 
 export type AnswerId = 'a' | 'b' | 'c';
 
